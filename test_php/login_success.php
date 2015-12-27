0dcdc361e468ef207d9ff4cb17246951e2a9ff12
gexing111 <gexing111@126.com>
2015-12-27 12:02:02 +0800

    add test php sessions from different devices

--- test_php/login_success.php
+++ test_php/login_success.php
@@ -4,11 +4,29 @@ session_start();
 if(isset($_SESSION['USERNAME']))
 {
 	//判断是否是同一个IP，近似判断是否是同一个设备
-	if($_SERVER['REMOTE_ADDR']     !== $_SESSION['LAST_REMOTE_ADDR'] ||
-	   $_SERVER['HTTP_USER_AGENT'] !== $_SESSION['LAST_USER_AGENT']) 
+	
+		echo "<br>";
+		echo strlen($_SERVER['REMOTE_ADDR']);
+		echo "<br>";
+		echo " CUR IP:".$_SERVER['REMOTE_ADDR'];
+		echo "<br>";
+		echo strlen($_SESSION['LAST_REMOTE_ADDR']);
+		echo "<br>";
+		echo" LAST IP:".$_SESSION['LAST_REMOTE_ADDR'];
+		echo "<br>";
+		echo strlen($_SERVER['HTTP_USER_AGENT']);
+		echo "<br>";
+		echo " CUR AG:".$_SERVER['HTTP_USER_AGENT'];
+		echo "<br>";
+		echo strlen($_SESSION['HTTP_USER_AGENT']);
+		echo "<br>";
+		echo " LAST AG:".$_SESSION['HTTP_USER_AGENT'];
+		echo "<br>";
+	if(($_SERVER['REMOTE_ADDR']     !== $_SESSION['LAST_REMOTE_ADDR'])||
+	   ($_SERVER['HTTP_USER_AGENT'] !== $_SESSION['HTTP_USER_AGENT'])) 
 	{
 
-		echo "you come from diffent IP, session destroyed! ";
+		echo "you come from diffent IP, session destroyed!";
    		session_destroy();
 	}
 	else
--- test_php/login_verify.php
+++ test_php/login_verify.php
@@ -1,33 +1,88 @@
 <?php
+
+function printInfo($cur_sessionkey, $last_sessionkey) {
+        echo "<br><br>";
+        echo " CUR  SESSION KEY:".$cur_sessionkey."<br>";
+        echo " LAST SESSION KEY:".$last_sessionkey."<br>";
+        echo " CUR  REMOTE ADDR:".$_SERVER['REMOTE_ADDR']."<br>";
+        echo"  LAST REMOTE ADDR:".$_SESSION['LAST_REMOTE_ADDR']."<br>";
+        echo " CUR  AGENT      :".$_SERVER['HTTP_USER_AGENT']."<br>";
+        echo " LAST AGENT      :".$_SESSION['HTTP_USER_AGENT']."<br>";
+}
+
+
 //获取表单信息
 $username = $_POST['username'];
 $password = md5($_POST['password']);
 
 //连接数据库
-@mysql_connect("localhost","","")
-or die("connect to db fail!");
-@mysql_select_db("mydb")
-or die("connect to db fail!");
+@mysql_connect("localhost","root","")
+or die("connect to db fail!<br>");
+@mysql_select_db("php_db")
+or die("select to db php_db fail!<br>");
 
 //获取db数据
-$query = @mysql_query("select username, password, last_remote_addr， http_user_agent from users where username = '$username' and password = '$password'")
-or die("SQL语句执行失败");
+$query = @mysql_query("select username, password, sessionkey from users where username = '$username'")
+or die("process query select fail!");
 
 //判断用户以及密码
 if($row = mysql_fetch_array($query))
 {
     session_start();
+    $sessionkey = session_id();
 
-    //初始化_SESSION
-    $_SESSION['USERNAME']           = $row['username'];
-    $_SESSION['PASSWORD']           = $row['password'];
-    $_SESSION['LAST_REMOTE_ADDR']   = $row['last_remote_addr'];
-    $_SESSION['HTTP_USER_AGENT']    = $row['http_user_agent'];
+    //验证用户名密码
+    if($username!==$row['username'] || $password !== md5($row['password']))
+    {
+        echo "verify password fail!<br>";
+        return ;
+    }
+    
 
-    echo "<a href='login_sucess.php'> welcome for tes php server!</a>";
+    echo "verify password success!<br>";
+    //session存在
+    if(isset($_SESSION['LAST_REMOTE_ADDR']))
+    {
+        //与上次同一个device
+        if(($sessionkey == $row['sessionkey'])&&
+           ($_SESSION['LAST_REMOTE_ADDR'] == $_SERVER['REMOTE_ADDR'])&&
+           ($_SESSION['HTTP_USER_AGENT']  == $_SERVER['HTTP_USER_AGENT'])) 
+        {
+
+            echo "you come from same IP!<br>";
+            printInfo($sessionkey, $row['sessionkey']);
+        }
+        //与上次不同设备
+        else
+        {
+
+            //更新数据
+            $_SESSION['LAST_REMOTE_ADDR']   = $_SERVER['REMOTE_ADDR'];
+            $_SESSION['HTTP_USER_AGENT']    = $_SERVER['HTTP_USER_AGENT'];
+            @mysql_query("update users set sessionkey='$sessionkey' where username = '$username'")
+            or die("process query update fail!");
+
+            echo "you come from diffent IP, session destroyed!<br>";
+            printInfo($sessionkey, $row['sessionkey']);
+            session_destroy();
+        }
+    }
+    //session不存在
+    else
+    {
+            //更新数据
+            $_SESSION['LAST_REMOTE_ADDR']   = $_SERVER['REMOTE_ADDR'];
+            $_SESSION['HTTP_USER_AGENT']    = $_SERVER['HTTP_USER_AGENT'];
+            @mysql_query("update users set sessionkey='$sessionkey' where username = '$username'")
+            or die("process query update fail!");
+
+            echo "you come for the first time!<br>";
+    }
+
 }
 else
 {
-    echo "<a href='login_fail.php'> verify error!";
+
+    echo "verify user fail!<br>";
 }
 ?>
